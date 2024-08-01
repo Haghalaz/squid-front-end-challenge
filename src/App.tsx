9216d78ffee149d137fdd80332b8c238d1a2e2ee
Luiz Coelho <luiz.adalberto@selettra.com.br>
2024-08-01 18:00:29 -0300

    feat: Add the image visualization

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { useEffect, useState } from "react";
-
+import { useEffect } from "react";
 import useSWR from "swr";
 
+import { AtSign, Calendar, Heart, MessageCircle } from "lucide-react";
+
 function App() {
-  const fetcher = (url: string): Promise<DataResponse> =>
+  const fetcher = (url: string): Promise<DataResponse[]> =>
     fetch(url).then((res) => res.json());
 
-  const { data, error, isLoading } = useSWR(
+  const {
+    data,
+    error,
+    isLoading,
+  }: { data: DataResponse[]; error: boolean; isLoading: boolean } = useSWR(
     "https://us-central1-squid-apis.cloudfunctions.net/test-front-basic",
     fetcher,
   );
@@ -17,22 +22,38 @@ function App() {
     console.log(`Error: `, error);
   }, [data, error, isLoading]);
 
-  const [count, setCount] = useState(0);
-
   return (
-    <main>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <main className="px-32">
+      <img className="w-72 my-4 mx-auto" src="/logo.svg" alt="Squit It Logo" />
+
+      {data && (
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-5">
+          {data.map((item) => (
+            <div className="relative w-full aspect-square overflow-hidden">
+              <img
+                className="size-full"
+                src={item.imagens.thumbnail.url}
+                alt="Squit It Logo"
+              />
+
+              <div className="absolute top-0 size-full bg-stone-700/80">
+                <Heart />
+                <MessageCircle />
+                <Calendar />
+                <AtSign />
+
+                {item.upvotes}
+                {item.criadoEm}
+                {item.comentarios}
+
+                {item.metadados.users_in_photo?.map((user) => (
+                  <p>{user.user.username}</p>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </main>
   );
 }
