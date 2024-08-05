9829c74427a376b42377a0039e4d377ae23f94b9
Luiz Coelho <luiz.adalberto@selettra.com.br>
2024-08-05 15:29:14 -0300

    feat: Optimize imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import Header from "./components/atoms/header";
-import Feed from "./components/organisms/feed";
+import { Loader2 } from "lucide-react";
+import { Suspense, lazy } from "react";
+
+const Header = lazy(() => import("@atoms/header"));
+const Feed = lazy(() => import("@organisms/feed"));
 
 function App() {
   return (
     <main className="px-32 py-12 h-screen space-y-12 overflow-y-scroll">
-      <Header />
-      <Feed />
+      <Suspense fallback={<Loader2 className="animate-spin mx-auto" />}>
+        <Header />
+      </Suspense>
+
+      <Suspense fallback={<Loader2 className="animate-spin mx-auto" />}>
+        <Feed />
+      </Suspense>
     </main>
   );
 }
