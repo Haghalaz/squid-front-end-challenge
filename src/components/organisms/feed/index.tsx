90aefbe69beab4c31c36de9d7fcfdaa8eeef2670
Luiz Coelho <luiz.adalberto@selettra.com.br>
2024-08-06 00:44:04 -0300

    style: Remove unused tag

--- src/components/organisms/feed/index.tsx
+++ src/components/organisms/feed/index.tsx
@@ -15,9 +15,7 @@ export default function Feed() {
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5">
       {data.map((photo) => (
-        <span key={photo._id}>
-          <PhotoCard data={photo} />
-        </span>
+        <PhotoCard key={photo._id} data={photo} />
       ))}
     </div>
   );
