8636d8354cf487a4a50a3dd6e8cdcdde93fc92f8
Luiz Coelho <luiz.adalberto@selettra.com.br>
2024-08-06 00:42:11 -0300

    small changes

--- src/components/atoms/header/index.tsx
+++ src/components/atoms/header/index.tsx
@@ -3,7 +3,7 @@ import Logo from "@assets/img/logo.svg";
 export default function Header() {
   return (
     <header>
-      <img className="w-72 mx-auto" src={Logo as string} alt="Squit It Logo" />
+      <img className="w-72 mx-auto" src={Logo as string} alt="Squid It Logo" />
     </header>
   );
 }
